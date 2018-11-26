f5f82164f94b06d2f803c0e3fb97d5a20abc86dd
Emmanuel Pinault <emmanuel.pinault@outreach.io>
2018-11-26 14:25:13 -0800

    Adds few fixes found in other forks
    Allow credentials to be passed up to allow securing behind phoenix
    fallback to longpolling over https if on https

--- src/js/wobserver_api_fallback.js
+++ src/js/wobserver_api_fallback.js
@@ -1,8 +1,22 @@
 function build_url(host, command, node = "") {
-  if( node == "local" ){
-    return 'http://' + host + '/api/' + encodeURI(command).replace(/#/g, '%23');
+  if (node == "local") {
+    return (
+      location.protocol +
+      "//" +
+      host +
+      "/api/" +
+      encodeURI(command).replace(/#/g, "%23")
+    );
   } else {
-    return 'http://' + host + '/api/' + encodeURI(node) + "/" + encodeURI(command).replace(/#/g, '%23');
+    return (
+      location.protocol +
+      "//" +
+      host +
+      "/api/" +
+      encodeURI(node) +
+      "/" +
+      encodeURI(command).replace(/#/g, "%23")
+    );
   }
 }
 
@@ -14,29 +28,33 @@ class WobserverApiFallback {
   }
 
   command(command, data = null) {
-    fetch(build_url(this.host, command, this.node))
+    fetch(build_url(this.host, command, this.node));
   }
 
   command_promise(command, data = null) {
-    return fetch(build_url(this.host, command, this.node))
-    .then(res => res.json())
-    .then(data => { return {
-      data: data,
-      timestamp: Date.now() / 1000 | 0,
-      type: command,
-    } } )
-    .then(e => {
-      if( !this.connected ){
-        this.connected = true;
-        this.on_reconnect()
-      }
-
-      return e;
+    return fetch(build_url(this.host, command, this.node), {
+      credentials: "same-origin"
     })
-    .catch(_ => {
-      this.connected = false;
-      this.on_disconnect();
-    });
+      .then(res => res.json())
+      .then(data => {
+        return {
+          data: data,
+          timestamp: (Date.now() / 1000) | 0,
+          type: command
+        };
+      })
+      .then(e => {
+        if (!this.connected) {
+          this.connected = true;
+          this.on_reconnect();
+        }
+
+        return e;
+      })
+      .catch(_ => {
+        this.connected = false;
+        this.on_disconnect();
+      });
   }
 
   set_node(node) {
@@ -44,4 +62,4 @@ class WobserverApiFallback {
   }
 }
 
-export{ WobserverApiFallback }
+export { WobserverApiFallback };
