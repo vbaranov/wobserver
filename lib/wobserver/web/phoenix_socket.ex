002c88451ed6a5d0af8f3d4aff52a42e529ecd46
Luke Imhoff <Kronic.Deth@gmail.com>
2019-01-18 08:53:52 -0600

    Wobserver.Web.PhoenixSocket.child_spec
    Needed for Phoenix 1.4 compatibility.

--- lib/wobserver/web/phoenix_socket.ex
+++ lib/wobserver/web/phoenix_socket.ex
@@ -13,11 +13,16 @@ defmodule Wobserver.Web.PhoenixSocket do
   """
 
   @doc false
+  def child_spec(opts) do
+    Phoenix.Socket.__child_spec__(__MODULE__, opts)
+  end
+
+  @doc false
   def __transports__ do
     config = [
       cowboy: Wobserver.Web.Client
     ]
-    callback_module = Wobserver.Web.PhoenixSocket
+    callback_module = __MODULE__
     transport_path = :ws
     websocket_socket = {transport_path, {callback_module, config}}
     # Only handling one type, websocket, no longpolling or anything else
